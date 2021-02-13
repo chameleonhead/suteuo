e017b5f48a66c549ff280272381dcd486f94e9c5
永野ゆうと <chamelion@hotmail.co.jp>
2021-02-13 17:50:28 +0900

    開発環境向上

--- amplify/backend/function/suteuomessaging/src/app.js
+++ amplify/backend/function/suteuomessaging/src/app.js
@@ -15,6 +15,26 @@ if (process.env.ENDPOINT_OVERRIDE) {
     accessKeyId: "fakeAccessKeyId",
     secretAccessKey: "fakeSecretAccessKey",
   });
+
+  //swaggerの基本定義
+  var options = {
+    swaggerDefinition: {
+      info: {
+        title: "suteuomessaging",
+        version: "1.0.0.",
+      },
+    },
+    apis: ["./app.js"], //自分自身を指定。外部化した場合は、そのファイルを指定。配列で複数指定も可能。
+  };
+
+  var swaggerJSDoc = require("swagger-jsdoc");
+  var swaggerSpec = swaggerJSDoc(options);
+
+  //swagger-ui向けにjsonを返すAPI
+  app.get("/api-docs.json", function (req, res) {
+    res.setHeader("Content-Type", "application/json");
+    res.send(swaggerSpec);
+  });
 }
 const docClient = new AWS.DynamoDB.DocumentClient();
 
@@ -22,6 +42,17 @@ function id() {
   return Math.random().toString(36).substring(2) + Date.now().toString(36);
 }
 
+/**
+ * @swagger
+ * /messaging:
+ *   get:
+ *     description: List messages
+ *     produces:
+ *       - application/json
+ *     responses:
+ *       200:
+ *         description: Success
+ */
 app.get("/messaging", function (req, res) {
   var params = {
     TableName: process.env.STORAGE_SUTEUOMESSAGING_NAME,
@@ -32,6 +63,25 @@ app.get("/messaging", function (req, res) {
   });
 });
 
+/**
+ * @swagger
+ * /messaging:
+ *   post:
+ *     description: Create message
+ *     consumes:
+ *       - application/json
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - name: body
+ *         description: Message body.
+ *         in: formData
+ *         required: true
+ *         type: string
+ *     responses:
+ *       200:
+ *         description: Success
+ */
 app.post("/messaging", function (req, res) {
   var params = {
     TableName: process.env.STORAGE_SUTEUOMESSAGING_NAME,
