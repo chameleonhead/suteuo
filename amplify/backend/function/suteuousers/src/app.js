90614f2bf557d122faaecaf26a71aba74c6a41eb
Yuto Nagano <chamelion@hotmail.co.jp>
2021-02-18 22:05:57 +0900

    ユーザー情報の登録処理を実装

--- amplify/backend/function/suteuousers/src/app.js
+++ amplify/backend/function/suteuousers/src/app.js
@@ -7,36 +7,62 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(awsServerlessExpressMiddleware.eventContext());
 
-const AWS = require("aws-sdk");
-const authClient = new AWS.CognitoIdentityServiceProvider();
+var UsersApi = require("./usersApi");
+const api = new UsersApi();
 
-app.get("/users/:userId", function (req, res) {
-  authClient.adminGetUser(
-    {
-      UserPoolId: process.env.AUTH_SUTEUO_USERPOOLID,
-      Username: req.params.userId,
+if (process.env.ENDPOINT_OVERRIDE) {
+  //swaggerの基本定義
+  var options = {
+    swaggerDefinition: {
+      info: {
+        title: "suteuousers",
+        version: "1.0.0.",
+      },
     },
-    function (err, data) {
-      if (err) res.json({ err });
-      else {
-        function findAttr(attrName) {
-          const filtered = data.UserAttributes.filter(
-            (e) => e.Name === attrName
-          );
-          return filtered.length ? filtered[0].Value : null;
-        }
-        const id = findAttr("sub");
-        res.json({
-          id,
-          username: findAttr("preferred_username") || id,
-          displayName: findAttr("preferred_username") || id,
-          avatarUrl: findAttr("picture"),
-          area: findAttr("area"),
-          rating: findAttr("rating"),
-        });
-      }
-    }
-  );
+    apis: ["./app.js"], //自分自身を指定。外部化した場合は、そのファイルを指定。配列で複数指定も可能。
+  };
+
+  var swaggerJSDoc = require("swagger-jsdoc");
+  var swaggerSpec = swaggerJSDoc(options);
+
+  //swagger-ui向けにjsonを返すAPI
+  app.get("/api-docs.json", function (req, res) {
+    res.setHeader("Content-Type", "application/json");
+    res.send(swaggerSpec);
+  });
+}
+
+function handleError(res, error) {
+  res.status(500);
+  res.json({
+    statusCode: 500,
+    code: "UnknownException",
+    message: "Error occured while accessing database.",
+    error: error,
+  });
+}
+
+app.get("/users/:userId", async function (req, res) {
+  try {
+    const user = await api.getUser(req.params.userId);
+    res.json(user);
+  } catch (error) {
+    handleError(res, error);
+  }
+});
+
+app.post("/users", async function (req, res) {
+  try {
+    const result = await api.updateUser({
+      id: req.body.userId,
+      area: req.body.area,
+      username: req.body.username,
+      displayName: req.body.displayName,
+    });
+    res.json(result);
+  } catch (error) {
+    handleError(res, error);
+  }
 });
 
 app.listen(3000, function () {
